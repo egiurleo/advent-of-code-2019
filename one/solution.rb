593c00cc0ed884f9c0c3406ccc3c50e24e341685
Emily Giurleo <e.m.giurleo@gmail.com>
2019-12-01 19:14:04 -0500

    day one

---
+++ one/solution.rb
@@ -0,0 +1,23 @@
+# At the first Go / No Go poll, every Elf is Go until the Fuel Counter-Upper. They haven't determined the amount of fuel required yet.
+
+# Fuel required to launch a given module is based on its mass. Specifically, to find the fuel required for a module, take its mass, divide by three, round down, and subtract 2.
+
+# For example:
+
+# For a mass of 12, divide by 3 and round down to get 4, then subtract 2 to get 2.
+# For a mass of 14, dividing by 3 and rounding down still yields 4, so the fuel required is also 2.
+# For a mass of 1969, the fuel required is 654.
+# For a mass of 100756, the fuel required is 33583.
+# The Fuel Counter-Upper needs to know the total fuel requirement. To find it, individually calculate the fuel needed for the mass of each module (your puzzle input), then add together all the fuel values.
+
+# What is the sum of the fuel requirements for all of the modules on your spacecraft?
+
+fuel = File.open('input.txt')
+        .map(&:chomp)
+        .map(&:to_f)
+        .map do |mass|
+          (mass / 3.0).floor - 2
+        end
+        .reduce(&:+)
+
+print "The fuel requirement is #{fuel}."
