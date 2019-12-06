ce9f3c3164328a0d938a7190a4b5f979f2e9c691
Emily Giurleo <e.m.giurleo@gmail.com>
2019-12-05 19:40:03 -0500

    unbroke it

--- 5/solution.rb
+++ 5/solution.rb
@@ -96,35 +96,50 @@ end
 
 require 'byebug'
 memory = File.read('5/input.txt').chomp.split(',').map(&:to_i)
-# memory = [4, 3, 99, 100]
+
+# memory = [1, 5, 6, 4, 0, 98, 1] # addition
+# memory = [2, 5, 6, 4, 0, 33, 3] # multiplication
+# memory = [3, 2, 0] # input
+# memory = [4, 2, 99] # output
+
+# memory = ['0001', 5, 6, 4, 0, 98, 1] # addition
+# memory = ['01', 5, 6, 4, 0, 98, 1] # addition
+# memory = ['1101', 98, 1, 4, 0] # addition
+
+# memory = ['0002', 5, 6, 4, 0, 33, 3] # multiplication
+# memory = ['02', 5, 6, 4, 0, 33, 3]
+# memory = ['1102', 33, 3, 4, 0]
+
+# memory = ['104', 6, 99]
+# memory = ['004', 2, 99]
+# memory = ['04', 2, 99]
+
 
 position = 0
 while memory[position] != 99
   op = memory[position]
 
+  unless ['1','2', '3', '4'].include?(op)
+    op_arr = op.to_s.split('').reverse
+    op = op_arr.first.to_i
+    mode0 = op_arr[2].to_i
+    mode1 = op_arr[3].to_i
+  end
+
+  mode0 = 0 unless mode0
+  mode1 = 0 unless mode1
+
   case op
   when 1
-    operation = Addition.new(memory, position)
+    operation = Addition.new(memory, position, modes: [mode0, mode1])
   when 2
-    operation = Multiplication.new(memory, position)
+    operation = Multiplication.new(memory, position, modes: [mode0, mode1])
   when 3
     operation = Input.new(memory, position)
   when 4
-    operation = Output.new(memory, position)
+    operation = Output.new(memory, position, mode: mode0)
   else
-    op_arr = op.to_s.split('').reverse
-
-    if op_arr.first == '1'
-      operation = Addition.new(memory, position, modes: [op_arr[2].to_i,  op_arr[3].to_i])
-    elsif op_arr.first == '2'
-      operation = Multiplication.new(memory, position, modes: [op_arr[2].to_i,  op_arr[3].to_i])
-    elsif op_arr.first == '3'
-      operation = Input.new(memory, position)
-    elsif op_arr.first == '4'
-      operation = Output.new(memory, position, mode: op_arr[2].to_i)
-    else
-      raise "Unrecognized op code: #{op_arr.first}"
-    end
+    raise "Unrecognized op code: #{op_arr.first}"
   end
 
   operation.perform
