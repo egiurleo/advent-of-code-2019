7b6774d7e09bd2a873af8a4880a483baea4975aa
Emily Giurleo <e.m.giurleo@gmail.com>
2019-12-09 12:54:28 -0800

    re-implemented day 6 as BFS

--- 6/solution.rb
+++ 6/solution.rb
@@ -53,24 +53,28 @@ end
 orbits = $nodes.map { |_, node| node.score }.reduce(&:+)
 puts "The solution to part one is: #{orbits}"
 
-goal_node = $nodes['SAN']
+goal_node = $nodes['SAN'].orbit
 current_node = $nodes['YOU'].orbit
-def fewest_orbital_transfers(current_node, goal_node)
-  current_node.see
 
-  if current_node.orbiters.include?(goal_node)
-    return 0
+def fewest_orbital_transfers(start_node, goal_node)
+  current_node_info = { node: start_node, level: 0 }
+  queue = []
+
+  while current_node_info[:node] != goal_node
+    current_node = current_node_info[:node]
+    current_node.see
+
+    next_nodes = [current_node.orbit].concat(current_node.orbiters).compact.filter{ |node| !node.seen }
+    next_nodes_info = next_nodes.map { |node| { node: node, level: current_node_info[:level] + 1 } }
+
+    queue.concat(next_nodes_info)
+
+    return Float::INFINITY if queue.empty?
+
+    current_node_info = queue.shift
   end
 
-  stack = [current_node.orbit].concat(current_node.orbiters).compact.filter{ |node| !node.seen }
-
-  if stack.empty?
-    return Float::INFINITY
-  end
-
-  result = stack.map { |node| fewest_orbital_transfers(node, goal_node) + 1 }.min
-
-  return result
+  return current_node_info[:level]
 end
 
 puts "The solution to part two is: #{fewest_orbital_transfers(current_node, goal_node)}"
