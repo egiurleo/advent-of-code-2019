da4336381f19a9d48fb94fd13c3a340925e14204
Emily Giurleo <e.m.giurleo@gmail.com>
2019-12-15 10:09:56 -0500

    multi-threaded version of day 13, part 2

--- 13/part2.rb
+++ 13/part2.rb
@@ -18,6 +18,7 @@ class Game
   end
 
   def draw
+    system "stty gfmt1:cflag=4b00:iflag=2b02:lflag=200005cb:oflag=3:discard=f:dsusp=19:eof=4:eol=ff:eol2=ff:erase=7f:intr=3:kill=15:lnext=16:min=1:quit=1c:reprint=12:start=11:status=14:stop=13:susp=1a:time=0:werase=17:ispeed=38400:ospeed=38400"
     puts "***** SCORE: #{@score} *****"
     @grid.each do |col|
       draw = col.map do |cell|
@@ -29,7 +30,7 @@ class Game
         when 2
           '#'
         when 3
-          '_'
+          'T'
         when 4
           '*'
         else
@@ -40,6 +41,34 @@ class Game
       end
       puts draw.join('')
     end
+  end
+
+  def read_char
+    begin
+      # save previous state of stty
+      old_state = `stty -g`
+      # disable echoing and enable raw (not having to press enter)
+      system "stty raw -echo"
+      c = STDIN.getc.chr
+      # gather next two characters of special keys
+      if(c=="\e")
+        extra_thread = Thread.new{
+          c = c + STDIN.getc.chr
+          c = c + STDIN.getc.chr
+        }
+        # wait just long enough for special keys to get swallowed
+        extra_thread.join(0.00001)
+        # kill thread so not-so-long special keys don't wait on getc
+        extra_thread.kill
+      end
+    rescue => ex
+      puts "#{ex.class}: #{ex.message}"
+      puts ex.backtrace
+    ensure
+      # restore previous state of stty
+      system "stty #{old_state}"
+    end
+    return c
   end
 
   def play
@@ -53,16 +82,6 @@ class Game
         if x == -1 && y == 0
           @score = item
         else
-          byebug if y >= HEIGHT || x >= WIDTH
-          if item == 3
-            # @grid[@paddle_pos.first][@paddle_pos.last] = nil if @paddle_pos
-            # @paddle_pos = [y, x]
-          elsif item == 4
-            # byebug
-            # @grid[@ball_pos.first][@ball_pos.last] = nil if @ball_pos
-            # @grid[x][y] = nil if @grid[x][y] == 2
-            # @ball_pos = [y, x]
-          end
           @grid[y][x] = item
         end
 
@@ -70,25 +89,27 @@ class Game
       end
 
       draw
-      input = nil
 
-      print "Joystick direction: "
-
-      while !['j', 'k', 'l'].include?(input)
-        input = gets.chomp
-
-        program_input = case input
-        when 'j'
-          -1
-        when 'k'
-          0
-        when 'l'
-          1
-        else
-          print "Invalid program input. Try again: "
+      input_thread = Thread.new do
+        input = read_char
+        if input == "\e[C"
+          Thread.current[:output] = 1
+        elsif input == "\e[D"
+          Thread.current[:output] = -1
+        elsif input == "e"
+          Thread.current[:output] = "xxx"
         end
+      end
 
-        @program.input([program_input])
+      t = input_thread.join(0.25)
+      input_thread.kill
+      if t.nil?
+        @program.input([0])
+      else
+        output = input_thread[:output]
+        break if output == "xxx"
+
+        @program.input([output])
       end
     end
   end
